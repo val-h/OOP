bbfdba2d3ebeb754bb920e0cceb1180b86995d17
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-02-22 03:15:08 +0000

    slight progress

--- SoftUni Exercises/Closest Two Points.py
+++ SoftUni Exercises/Closest Two Points.py
@@ -1,12 +1,25 @@
-n = int(input('Number of points: '))
-points = []
-closest = ''
+import sys
+import math
+n = int(input('Times to repeat: '))
+points = {}
+diff = sys.maxsize
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def Distance(p1, p2):
+    a = abs(p1.x - p2.x)
+    b = abs(p1.y - p2.y)
+    c = pow(a, 2) + pow(b, 2)
+    return math.sqrt(c)
+
 
 for i in range(0, n):
-    points.append(input(f'Points n {i + 1}: '))
+    usrInp = input('x and y - ')
+    x, y = [int(x) for x in usrInp.split()]
+    points[i] = x, y
 
-def ClosestPoints(arr):
-    pass
-
-
-print(ClosestPoints(points))
+# TODO > finish the distance logic
--- SoftUni Exercises/Distance Between Points.py
+++ SoftUni Exercises/Distance Between Points.py
@@ -1,21 +1,25 @@
 import math
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+def Distance(f, s):
+    a = f.x - s.x
+    b = f.y - s.y
+    c = pow(a, 2) + pow(b, 2)
+    return math.sqrt(c)
+
 fPoints = input('x1, y1 = ')
 sPoints = input('x2, y2 = ')
 x1, y1 = [int(x) for x in fPoints.split()]
 x2, y2 = [int(x) for x in sPoints.split()]
 
-a = abs(x1 - x2)
-b = abs(y1 - y2)
+p1 = Point(x1, y1)
+p2 = Point(x2, y2)
 
+dist = Distance(p1, p2)
 
-class Point:
-
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-    
-    def Distance(self):
-        c = math.pow(a, 2) + math.pow(b, 2)
-        return math.sqrt(c)
-
-result = Point.
+print(f'Distance is {dist:.3f}')
