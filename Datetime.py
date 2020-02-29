67af5aca13c6d0701b64d21f12d2af48d89ea94c
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-02-29 02:38:16 +0000

    Working... almost as intended

--- Datetime.py
+++ Datetime.py
@@ -1,7 +1,5 @@
 from datetime import datetime
 
-rnd_date = '29.07.1954'
+parsed_date = datetime.strptime(input('Target date: '), "%d.%m.%Y")
 
-parsed_date = datetime.strptime(rnd_date, "%d.%m.%Y")
-
-print(parsed_date)
+print(parsed_date.strftime('%d %m %Y'))
---
+++ SoftUni Exercises/Andrey and Billard.py
@@ -0,0 +1,48 @@
+number_of_products = int(input('Amount of products: '))
+product_prices = {}
+customers = []
+total_bill = 0.0
+
+class Customer:
+
+    bill = 0.0
+
+    def __init__(self, name, product, quantity):
+        
+        self.orders = {}
+
+        self.name = name
+        self.product = product
+        self.quantity = int(quantity)
+        if product not in self.orders: self.orders[product] = quantity
+        else: self.orders[product] += quantity        
+        self.bill += self.quantity * product_prices[self.product]
+
+    def PrintOrders(self):
+        print(self.name)
+        for prod, quant in sorted(self.orders.items(), key=lambda item: item[0]):
+            print(f'-- {prod} - {quant}')
+        print(f'Bill: {self.bill}')
+
+# Receiving and storing the products
+for i in range(0, number_of_products):
+    user_inp = input(f'Product N{i+1}: ')
+    product = user_inp.split('-')
+    product_prices[product[0]] = float(product[1])
+
+# Recieving customer orders until 'end of clients' command is given
+while True:
+    cmd = input('/> ')
+    if cmd == 'end of clients': break
+    else:   # Creating a list of information from the cmd
+        cmd = cmd.replace('-', ' ').replace(',', ' ')
+        customer_order = cmd.split()
+        print(customer_order)
+        if customer_order[1] in product_prices.keys():  # Creating instances of Customer
+            customers.append(Customer(customer_order[0], customer_order[1], customer_order[2]))
+
+# Output
+for cust in sorted(customers, key=lambda x: x.name):
+    total_bill += cust.bill
+    print(cust.PrintOrders())
+print(f'Total bill: {total_bill}')
